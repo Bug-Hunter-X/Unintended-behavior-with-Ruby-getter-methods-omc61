4cadb4ceca47fa90a022c0891f6cdda9f710575f
BugHunter-X <askzenai@gmail.com>
2024-12-16 20:38:06 +0100

    Add bugSolution.rb file

---
+++ bugSolution.rb
@@ -0,0 +1,20 @@
+```ruby
+class MyClass
+  def initialize(value)
+    @value = value
+  end
+
+  def value
+    @value
+  end
+
+  def value=(new_value)
+    @value = new_value
+  end
+end
+
+my_object = MyClass.new(10)
+puts my_object.value #=> 10
+my_object.value = 20 #This will now correctly modify @value
+puts my_object.value #=> 20
+```
